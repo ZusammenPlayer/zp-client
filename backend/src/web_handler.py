3787e174d4764b3786520e7e7ea18f7fc95df27b
Markus Schubert <markus@toto.io>
2023-12-19 21:17:43 +0100

    add endpoint for getting media files

--- backend/src/web_handler.py
+++ backend/src/web_handler.py
@@ -1,7 +1,9 @@
 from aiohttp import web
+import json
 
 def setup(app: web.Application, conf):
     app.router.add_get("/", index_handler)
+    app.router.add_get('/media/{t}/{d}', get_media_file)
     app.router.add_static("/", conf["zp-client"]["web-path"], show_index=True, follow_symlinks=True)
     app.router.add_get('/{tail:(?!api).*}', index_handler)
 
@@ -11,3 +13,27 @@ async def index_handler(request):
     with open(conf["zp-client"]["web-path"] + "/index.html") as f:
         html = f.read()
         return web.Response(text=html, content_type="text/html")
+
+async def get_media_file(request):
+    conf = request["conf"]
+
+    with open(conf["zp-client"]["data-path"] + "/db.json", "r") as db_file:
+        database = json.load(db_file)
+        mime_type = None
+        filename = None
+        if request.match_info["t"] == "md5":
+            for m in database["media"]:
+                if m["md5"] == request.match_info["d"]:
+                    mime_type = m["mime_type"]
+                    filename = m["filename"]
+        if request.match_info["t"] == "file":
+            for m in database["media"]:
+                if m["filename"] == request.match_info["d"]:
+                    mime_type = m["mime_type"]
+                    filename = m["filename"]
+
+        if filename is not None and mime_type is not None:
+            with open(conf["zp-client"]["data-path"] + "/" + filename, "rb") as media_file:
+                return web.Response(body=media_file.read(), content_type=mime_type)
+        else:
+            return web.HTTPNotFound()
